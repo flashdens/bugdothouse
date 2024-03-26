e5f9bf1bb370f12be06e0aa9742e474a64d7bf77
flashdens <metis234@gmail.com>
2024-03-26 12:05:04 +0100

    changed db to postgres and applied test migration

--- server/bugdothouse/settings.py
+++ server/bugdothouse/settings.py
@@ -75,10 +75,13 @@ WSGI_APPLICATION = 'bugdothouse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bugdothouse',
+        'USER': 'bugdothouse',
+        'PASSWORD': 'bugdothouse',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }}
 
 
 # Password validation
