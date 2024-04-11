574777ffaaa8064f4e5228f9d8a5255181efba1a
flashdens <metis234@gmail.com>
2024-04-11 12:32:09 +0200

    goodbye __pycache__ from my git repo

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,7 +2,15 @@ import {Chessboard} from "react-chessboard";
 
 const Index = () => {
     return (
-        <Chessboard/>
+        <div>
+            <div style={{ width: '80dvh'}}>
+                <Chessboard
+                    id={'basicChessboard'}
+                    position="r1bqkbnr/pppppppp/2n5/8/4P3/8/PPPP1PPP/RNBQKBNR"
+                    arePremovesAllowed={true}
+                />
+            </div>
+        </div>
     )
 }
 
