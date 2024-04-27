3e746f3dd04bbf52c7f8a0035ce59889cb03b3d0
flashdens <metis234@gmail.com>
2024-04-27 18:27:19 +0200

    altered game model to include players

--- server/game/models/game.py
+++ server/game/models/game.py
@@ -1,7 +1,14 @@
 from django.db import models
-
+from .user import User
 
 class Game(models.Model):
-    status = models.CharField(max_length=20, default="ongoing")
+    STATUS_CHOICES = [
+        ("ongoing", "Ongoing"),
+        ("finished", "Finished"),
+    ]
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="ongoing")
     fen = models.CharField(max_length=71, default="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", null=False)
     side_to_move = models.CharField(default="WHITE", null=True)
+    white_player = models.ForeignKey(User, related_name='white_games', on_delete=models.SET_NULL, null=True)
+    black_player = models.ForeignKey(User, related_name='black_games', on_delete=models.SET_NULL, null=True)
