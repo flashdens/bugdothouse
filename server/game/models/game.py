956cdeb5769dc9f29a12cc744a5abf0915a98139
flashdens <metis234@gmail.com>
2024-04-30 14:33:01 +0200

    extended fen max length to handle crazyhouse fens

--- server/game/models/game.py
+++ server/game/models/game.py
@@ -8,7 +8,7 @@ class Game(models.Model):
     ]
 
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="ongoing")
-    fen = models.CharField(max_length=71, default="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", null=False)
+    fen = models.CharField(max_length=128, default="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", null=False)
     side_to_move = models.CharField(default="WHITE", null=True)
     white_player = models.ForeignKey(User, related_name='white_games', on_delete=models.SET_NULL, null=True)
     black_player = models.ForeignKey(User, related_name='black_games', on_delete=models.SET_NULL, null=True)
