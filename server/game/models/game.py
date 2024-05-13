55ed1b72387f8ee87c6b942a6d481a1457c4a094
flashdens <metis234@gmail.com>
2024-05-13 20:47:45 +0200

    added gamemode choices to game

--- server/game/models/game.py
+++ server/game/models/game.py
@@ -9,7 +9,14 @@ class Game(models.Model):
         ("finished", "Finished"),
     ]
 
+    GAMEMODE_CHOICES = [
+        ("crazyhouse", "Crazyhouse"),
+        ("bughouse", "Bughouse"),
+        ("classic", "Classic"),
+    ]
+
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="waiting_for_start")
+    gamemode = models.CharField(max_length=20, choices=GAMEMODE_CHOICES, default="waiting_for_start")
     fen = models.CharField(max_length=128, default="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
                            null=False)
     side_to_move = models.CharField(default="WHITE", null=True)
