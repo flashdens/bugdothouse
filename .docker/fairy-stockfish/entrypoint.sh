f449e184df53a03820cb81a2e4a9b58487fa78fa
flashdens <metis234@gmail.com>
2024-05-14 12:13:02 +0200

    configured ssh-server in fairy-stockfish container

--- .docker/fairy-stockfish/entrypoint.sh
+++ .docker/fairy-stockfish/entrypoint.sh
@@ -2,7 +2,9 @@
 set -e
 
 if [ -z "$1" ]; then
-  tcpserver -c2 0 23249 /stockfish/stockfish
+  /usr/sbin/sshd -D &
+  echo "ssh daemon is ready to accept connections"
+  tcpserver -c2 0 23249 /stockfish/stockfish &
   echo "fairy-stockfish TCP server is ready to accept connections"
 else
   # else default to run whatever the user wanted like "bash" or "sh"
--- tester.py
+++ tester.py
@@ -5,7 +5,7 @@ import chess.engine
 
 
 async def main() -> None:
-    async with asyncssh.connect("fairy_stockfish:23249") as conn:
+    async with asyncssh.connect("fairy_stockfish") as conn:
         channel, engine = await conn.create_subprocess(chess.engine.UciProtocol, "/stockfish/stockfish")
         await engine.initialize()
 
