bc6c44235fc0b13d2a996a029f75be699c9684df
flashdens <metis234@gmail.com>
2024-05-22 14:56:33 +0200

    added spectators fields to game

--- server/game/models/game.py
+++ server/game/models/game.py
@@ -20,8 +20,15 @@ class Game(models.Model):
     code = models.CharField(max_length=8, null=True, unique=True)
     is_private = models.BooleanField(default=False, null=False)
     host = models.ForeignKey(User, related_name='host', on_delete=models.SET_NULL, null=True)
-    fen = models.CharField(max_length=128, default="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-                           null=False)
-    side_to_move = models.CharField(default="WHITE", null=True)
+    fen = models.CharField(
+        max_length=128,
+        default="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+        null=False
+    )
+    side_to_move = models.CharField(max_length=10, default="WHITE", null=True)
     white_player = models.ForeignKey(User, related_name='white_games', on_delete=models.SET_NULL, null=True)
     black_player = models.ForeignKey(User, related_name='black_games', on_delete=models.SET_NULL, null=True)
+    spectators = models.ManyToManyField(User, related_name='spectators', blank=True)
+
+    def __str__(self):
+        return f"Game {self.code} - {self.status}"
