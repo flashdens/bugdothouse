c5a8f35d1c3a98bd8b6e8445aa01fb6215f86431
flashdens <metis234@gmail.com>
2024-05-29 09:31:23 +0200

    changed status and gamemode fields to ints as enums

--- server/game/models/game.py
+++ server/game/models/game.py
@@ -1,36 +1,46 @@
 from django.db import models
+from django.db.models.enums import IntEnum
+
 from authorization.models.user import User
 
 from enum import Enum
 
 
+class GameStatus(models.IntegerChoices):
+    WAITING_FOR_START = (0, 'Waiting for start')
+    ONGOING = (1, 'Ongoing')
+    FINISHED = (2, 'Finished')
+
+
+class GameModes(models.IntegerChoices):
+    CRAZYHOUSE = (0, 'Crazyhouse')
+    BUGHOUSE = (1, 'Bughouse')
+    CLASSICAL = (2, 'Classical')
+
+
+class GameOutcomes(models.IntegerChoices):
+    WHITE_WIN = (0, 'White Wins')
+    BLACK_WIN = (1, 'Black Wins')
+    TEAM_1_WIN = (2, 'Team 1 Wins')
+    TEAM_2_WIN = (3, 'Team 2 Wins')
+    DRAW = (4, 'Draw')
+
+
+SUBGAME_ID_CHOICES = [
+    (1, "1"),
+    (2, "2")
+]
+
+
 class Game(models.Model):
-    STATUS_CHOICES = [
-        ("waiting_for_start", "Waiting for start"),
-        ("ongoing", "Ongoing"),
-        ("finished", "Finished"),
-    ]
-
-    GAMEMODE_CHOICES = [
-        ("crazyhouse", "Crazyhouse"),
-        ("bughouse", "Bughouse"),
-        ("classic", "Classic"),
-    ]
-
-    SUBGAME_ID_CHOICES = [
-        (1, "1"),
-        (2, "2")
-    ]
-
-    class GameOutcomes(models.IntegerChoices):
-        WHITE_WIN = 0, 'White Wins'
-        BLACK_WIN = 1, 'Black Wins'
-        TEAM_1_WIN = 2, 'Team 1 Wins'
-        TEAM_2_WIN = 3, 'Team 2 Wins'
-        DRAW = 4, 'Draw',
-
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="waiting_for_start")
-    gamemode = models.CharField(max_length=20, choices=GAMEMODE_CHOICES, default="crazyhouse", null=True)
+    status = models.IntegerField(
+        choices=GameStatus.choices,
+        default=GameStatus.WAITING_FOR_START
+    )
+    gamemode = models.IntegerField(
+        choices=GameModes.choices,
+        default=GameModes.CRAZYHOUSE
+    )
     code = models.CharField(max_length=6, null=True, unique=False)
     is_private = models.BooleanField(null=False)
     host = models.ForeignKey(User, related_name='host', on_delete=models.SET_NULL, null=True)
